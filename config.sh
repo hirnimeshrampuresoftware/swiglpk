a8eeac60c0c193a0fe4f3bd179beb507418fc7af
hirnimeshrampuresoftware <71631645+hirnimeshrampuresoftware@users.noreply.github.com>
2022-02-02 16:59:07 +0530

    Update config.sh

--- config.sh
+++ config.sh
@@ -22,7 +22,7 @@ function pre_build {
 				&& make \
 				&& make install)
 		pip3 install requests
-    export NEW_GLPK_VERSION=$(python scripts/find_newest_glpk_release.py)
+    export NEW_GLPK_VERSION=$(python3 scripts/find_newest_glpk_release.py)
 	fi
 	echo "Downloading http://ftp.gnu.org/gnu/glpk/glpk-$NEW_GLPK_VERSION.tar.gz"
     curl -O "http://ftp.gnu.org/gnu/glpk/glpk-$NEW_GLPK_VERSION.tar.gz"
